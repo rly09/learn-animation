7373542b7d2d2b80eb45f94c58c65970aefa4294
Roshan lal yogi <yogiroshan2005@gmail.com>
2025-08-20 21:31:16 +0530

    Initial Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'dart:math' show pi;
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 2),
+    );
+    _animation:
+    Tween<double>(
+      begin: 0.0,
+      end: 2 * pi,
+    ).animate(_controller);
+    _controller.repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _controller,
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..rotateY(
+                  _animation.value,
+                ),
+              child: Container(
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      blurRadius: 10,
+                      offset: Offset(0, 5),
+                    ),
+                  ],
+                  color: Colors.green,
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
