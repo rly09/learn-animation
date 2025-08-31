dc0d7254b2d1907c7778e06e9bd5ddbb36a79e05
Roshan lal yogi <yogiroshan2005@gmail.com>
2025-08-31 10:35:14 +0530

    Improving

--- lib/home_page1.dart
+++ lib/home_page1.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:math' show pi;
 
 enum CircleSide {
   left,
   right,
 }
 
-extension ToPath on CircleSide{
-  Path toPath(Size size){
+extension ToPath on CircleSide {
+  Path toPath(Size size) {
     final path = Path();
 
     late Offset offset;
     late bool clockwise;
 
-    switch (this){
+    switch (this) {
       case CircleSide.left:
         path.moveTo(size.width, 0);
         offset = Offset(size.width, size.height);
@@ -24,16 +25,53 @@ extension ToPath on CircleSide{
         break;
     }
     path.arcToPoint(offset,
-      radius: Radius.elliptical(size.width/2, size.height/2),
-      clockwise: clockwise
-    );
+        radius: Radius.elliptical(size.width / 2, size.height / 2),
+        clockwise: clockwise);
     path.close();
     return path;
   }
 }
 
-class HomePage1 extends StatelessWidget {
+class HalfCircleClipper extends CustomClipper<Path> {
+  final CircleSide side;
+
+  HalfCircleClipper({
+    required this.side,
+  });
+
+  @override
+  Path getClip(Size size) => side.toPath(size);
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+}
+
+class HomePage1 extends StatefulWidget {
   const HomePage1({super.key});
+
+  @override
+  State<HomePage1> createState() => _HomePage1State();
+}
+
+class _HomePage1State extends State<HomePage1> with  SingleTickerProviderStateMixin{
+
+  late AnimationController _counterClockwiseRotationController;
+  late Animation<double> _counterClockwiseRotationAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _counterClockwiseRotationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+
+    _counterClockwiseRotationAnimation = Tween<double>(
+      begin: 0,
+      end: -(pi/2)
+    ).animate(_counterClockwiseRotationController);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,19 +80,21 @@ class HomePage1 extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                  color: Colors.blue,
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(100),
-                      bottomLeft: Radius.circular(100))),
+            ClipPath(
+              clipper: HalfCircleClipper(side: CircleSide.left),
+              child: Container(
+                width: 100,
+                height: 100,
+                color: Colors.blue,
+              ),
             ),
-            Container(
-              color: Colors.yellow,
-              width: 100,
-              height: 100,
+            ClipPath(
+              clipper: HalfCircleClipper(side: CircleSide.right),
+              child: Container(
+                color: Colors.yellow,
+                width: 100,
+                height: 100,
+              ),
             ),
           ],
         ),
